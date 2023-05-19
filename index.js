4ed463b00ad181268e5b41b421b364b740efc340
Deteor <deadman.deteor2209@gmail.com>
2023-05-19 17:59:45 -0600

    Añadiendo funcionalidad desencriptar a la aplicación

--- index.js
+++ index.js
@@ -23,3 +23,29 @@ function encriptar() {
         alert("Debes ingresar algun texto");  
     }
 }
+
+function desencriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muñeco = document.getElementById("muñeco");
+
+    let textoCifrado = texto
+        .replace(/enter/gi, "e")
+        .replace(/imes/gi, "i")
+        .replace(/ai/gi, "a")
+        .replace(/ober/gi, "o")
+        .replace(/ufat/gi, "u");
+
+    if (texto.length != 0) {
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto desencriptado con exito";
+        parrafo.textContent = "";
+        muñeco.src = "./img/desencriptado.png";
+    } else {
+        muñeco.src = "./img/muñeco.png";
+        tituloMensaje.textContent = "Ningún mensaje fue encontrado"
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar"
+        alert("Debes ingresar algun texto");  
+    }    
+}
